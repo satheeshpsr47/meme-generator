fed0509355b38f47bdf813e48f5a4d2bad3041e2
satheesh <90098687+satheeshpsr47@users.noreply.github.com>
2023-06-11 11:15:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Memes from './components/memes';
+import Box from './components/box';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome broh</h1>
+      <Memes />
+      <Box />
+    </div>
+  );
+}
+
+export default App;
